b269b9c8c740f1634a729cf2977539b93fc97e94
Shubham Singh <shubham88singh@gmail.com>
2020-01-22 23:05:34 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,11 +16,6 @@ function generateCard(card)
 {
     return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`
 }
-// var shake=document.createElement('link');
-// shake.rel='stylesheet';
-// shake.type='text/css';
-// shake.href="shake_effect.css";
-// document.getElementsByTagName('head')[0].appendChild()
 
 let move=0;
 
@@ -39,13 +34,6 @@ initGame();
 const allCards=document.querySelectorAll(".card");
 let openCards=[];
 let moveCounter=document.querySelector('.moves');
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -80,17 +68,6 @@ function moveCounter1()
 }
 
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 
 
